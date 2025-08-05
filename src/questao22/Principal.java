08bab552b594e14be9898077657e6ecc07863c2d
joaof <joaoferreira5691@gmail.com>
2025-08-05 19:19:40 -0300

    Questão 22 finalizada

---
+++ src/questao22/Principal.java
@@ -0,0 +1,14 @@
+package questao22;
+import java.util.Scanner;
+public class Principal {
+    public static void main(String args[]) {
+        Scanner entrada = new Scanner(System.in);
+        int num;
+        System.out.print("Digite um número inteiro:");
+        num=entrada.nextInt();
+        System.out.printf("%d*1=%d\n%d*2=%d\n%d*3=%d\n%d*4=%d\n%d*5=%d\n%d*6=%d\n%d*7=%d\n%d*8=%d\n%d*9=%d\n%d*10=%d\n", num,num*1,
+                num,num*2,num,num*3,num,num*4,num,num*5,num,num*6,num,num*7,num,num*8,num,num*9,num,num*10);
+        System.out.printf("%d/1=%d\n%d/2=%d\n%d/3=%d\n%d/4=%d\n%d/5=%d\n%d/6=%d\n%d/7=%d\n%d/8=%d\n%d/9=%d\n%d/10=%d\n", num,num*1,
+                num,num/2,num,num/3,num,num/4,num,num/5,num,num/6,num,num/7,num,num/8,num,num/9,num,num/10);
+    }
+}
